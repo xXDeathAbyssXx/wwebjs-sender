d94e02ca5fc05527639b31b842843e034fc862ab
DeathAbyss <deathabyss.dev@gmail.com>
2022-03-27 17:49:20 -0500

    Fixing sizeEmbed Beta Test

--- index.js
+++ index.js
@@ -111,10 +111,8 @@ exports.MessageEmbed = class MessageEmbed {
     let line_convert = null;
     if (typeof px === "number") {
       if (px > 2 && px < 47) {
-        let mathnum = px - 2;
-        let fixmath = Math.abs(mathnum);
         line_convert = "";
-        for (let i = 0; i < fixmath; i++) {
+        for (let i = 0; i < px; i++) {
           line_convert = line_convert + "─";
         }
         embedsize = {
