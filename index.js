928938becb38178d8334ca0b1ab732f587c5ed01
DeathAbyss <deathabyss.dev@gmail.com>
2022-04-21 11:02:12 -0500

    Fixing start collector event

--- index.js
+++ index.js
@@ -1606,7 +1606,7 @@ exports.Collector = class Collector extends EventEmitter {
     let message = await awaitMessage(client, time, chat);
 
     if (message) {
-      this.emit("message", message);
+      return this.emit("message", message);
     }
   }
 
